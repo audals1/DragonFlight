9bd768acffe85e0f06002a576ed88fbc11000b47
audals1 <97594593+audals1@users.noreply.github.com>
2024-09-08 17:12:57 +0900

    Update MakeLatterBox.cs

--- Assets/Script/UI/MakeLatterBox.cs
+++ Assets/Script/UI/MakeLatterBox.cs
@@ -6,16 +6,4 @@ public class MakeLatterBox : MonoBehaviour
 {
     [SerializeField]
     Texture2D m_texure;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-  
 }
